a0777db2cfe8174c4ec2805a47f31ca9a6e55245
prasad394 <prasad394@gmail.com>
2014-04-27 16:44:55 -0500

    Update to the matrix inverse assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions 
+## makeCacheMatrix and cacheSolve that help comupte and retrieve the inverse of the matrix from cache
 
-## Write a short comment describing this function
+## maxCacheMatrix takes a matrix as input and gives a vector of functions which set and get the original matrix and set and get the 
+## inverse of the matrix. It is assumed that the matrix is square and inversible
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function ( y)
+  {
+    y <<- x
+    m <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inv) m <<- solve(x)
+  getInv <- function()m
+  list (set=set,get=get,getInv=getInv,setInverse=setInverse)    
 }
 
-
-## Write a short comment describing this function
+## The cacheSolve function checks if the matrix input is the same as the previous one. If so, it returns the inverse from the cache.
+## If not it computes the inverse through the solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # check to see if the matrices have changed
+  m <- x$get
+  Inv <- x$getInv
+  if (x == m) {
+  # matrix hasn't changed return the inverse
+    message("Taking values from cache")
+    retrun(Inv)
+  }
+  m <- solve(x)  
+  x$setInverse(m)
+  m
 }
